aac080d546bf0d892d1891552108d21541fcce29
Nicolás Alliaume <nicolasalliaume@gmail.com>
2019-09-17 15:13:23 -0300

    ++ check if theme already exists and prompt user

--- commands/branch.js
+++ commands/branch.js
@@ -2,6 +2,7 @@ const createGitBranch = require( '../utils/create-git-branch' );
 const createTheme = require( '../utils/create-theme' );
 const getMatchingTheme = require( '../utils/get-matching-theme' );
 const syncTheme = require( '../utils/sync-theme' );
+const promptUser = require( 'command-prompt-user' );
 
 module.exports = async function( command ) {
 	const branchName = command[ '__' ][ 1 ];
@@ -13,42 +14,51 @@ module.exports = async function( command ) {
 		`Creating empty Shopify theme ${ branchName.bold.green }...`
 	);
 
-	const devTheme = await getMatchingTheme( 'dev' );
-	if ( ! devTheme ) {
-		throw new Error( 
-			`There's no ${ 'dev'.bold } theme present in the store.`.red 
-			+ `\nA dev theme is needed as base theme.`
-		);
-	}
+	const devTheme = await getThemeOrThrow( 'dev' );
 
-	const newTheme = await createTheme( branchName );
+	let newTheme = await getMatchingTheme( branchName );
+	if ( newTheme || !command.y ) {
+		const shouldContinue = promptUser( `Theme ${ newTheme.name.bold } already exists. Continue anyway?` );
+		if ( shouldContinue.toLowerCase() !== 'y' ) {
+			throw new Error( `Cancelled by user.` );
+		}
+		!command.silent && console.log( `Continuing...` );
+	}
+	else {
+		newTheme = await createTheme( branchName );
+		!command.silent && console.log( `Theme ${ newTheme.name.bold } created.`.green );
+	}
 
 	const assetsToCopy = [ 'config/settings_data.json' ];
 
-	!command.silent && console.log( `Theme ${ newTheme.name } created.`.green );
 	!command.silent && console.log( 
 		`Copying assets from theme ${ devTheme.name.bold } to theme ${ newTheme.name.bold }...`
 		+ `\n\t* ` + assetsToCopy.join( '\n\t* ' )
 	);
 
-	await syncTheme( 
-		devTheme.id, 
-		newTheme.id, 
-		assetsToCopy, 
-		command.silent, 
-		command.y 
-	);
+	await syncTheme( devTheme.id, newTheme.id, assetsToCopy, command.silent, command.y );
 
 	!command.silent && console.log(
 		`Creating git branch ${ branchName.bold.green }...`
 	);
 
-	await createGitBranch( branchName );
+	await createGitBranch( branchName, 'dev' );
 
 	!command.silent && console.log( `Created git branch ${ branchName.bold }`.green );
 
 	!command.silent && console.log( 
 		`Run ${ `yarn start`.gray } to upload the theme to Shopify and start working.`.blue 
 	);
+	
 	!command.silent && console.log( `✅ Done` );
 }
+
+
+async function getThemeOrThrow( name ) {
+	const devTheme = await getMatchingTheme( name );
+	if ( ! devTheme ) {
+		throw new Error( `There's no ${ name.bold } theme present in the store.`.red );
+	}
+	return devTheme;
+}
+
--- utils/create-git-branch.js
+++ utils/create-git-branch.js
@@ -2,7 +2,7 @@ const git = require( 'simple-git' )( process.cwd() );
 const branchExists = require( './branch-exists' );
 const checkoutBranch = require( './checkout-git-branch' );
 
-module.exports = async function( name ) {
+module.exports = async function( name, sourceBranch = 'dev' ) {
 	const exists = await branchExists( name );
 
 	if ( exists ) {
@@ -10,7 +10,7 @@ module.exports = async function( name ) {
 	}
 
 	return new Promise( ( resolve, reject ) => {
-		git.checkoutBranch( name, 'dev', ( error, result ) => {
+		git.checkoutBranch( name, sourceBranch, ( error, result ) => {
 			if ( error ) {
 				return reject( 
 					`Cannot create branch ${ name.green }.` 
